f6c546a284e37c8c260b49154fee5b34149f62cf
zoujiajia <zoujj@friendcom.com>
2018-01-11 08:44:00 +0800

    V0.2
    Signed-off-by: zoujiajia <zoujj@friendcom.com>

---
+++ 1/oem_version.c
@@ -0,0 +1,18 @@
+/*
+#============================================================================
+#  Name:                                                                     
+#    oem_version.c 
+#
+#  Description:                                                              
+#    None 
+#                                                                            
+# Copyright (c) 2018 by QUALCOMM, Incorporated.  All Rights Reserved.        
+#============================================================================
+#                                                                            
+# *** AUTO GENERATED FILE - DO NOT EDIT                                      
+#                                                                            
+# GENERATED: Wed Jan 10 13:42:52 2018 
+#============================================================================
+*/
+const char IMAGE_VARIANT_STRING_AUTO_UPDATED[]="IMAGE_VARIANT_STRING=ACINAAAZA";
+const char OEM_IMAGE_VERSION_STRING_AUTO_UPDATED[]="OEM_IMAGE_VERSION_STRING=DESKTOP-LT9EAF9";
